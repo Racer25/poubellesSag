4ed7d764bec9e61771810804442756d85aef4bef
Charles <char-cou@hotmail.fr>
2018-08-22 23:04:11 -0400

    Genelalisation of the garbage workflow

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ let cal;
 const CONFIG = require('./config.json');
 
 // Requests for https://ville.saguenay.ca/services-aux-citoyens/environnement/ordures-menageres/horaire-de-la-collecte
-let getRuesRequest = function (civicNumber) {
+let getRuesRequest = function (civicNumber)
+{
     return new Promise((resolve, reject) => {
         request
             .post(
@@ -29,7 +30,8 @@ let getRuesRequest = function (civicNumber) {
             );
     });
 };
-let getCollecteInfoRequest = function (idBatiment) {
+let getCollecteInfoRequest = function (idBatiment)
+{
     return new Promise((resolve, reject) => {
         request
             .post(
@@ -48,7 +50,8 @@ let getCollecteInfoRequest = function (idBatiment) {
             );
     });
 };
-let getCeduleRequest = function (jour, type) {
+let getCeduleRequest = function (jour, type)
+{
     return new Promise((resolve, reject) => {
         request
             .post(
@@ -69,7 +72,8 @@ let getCeduleRequest = function (jour, type) {
 };
 
 //Promises for calendar
-let promiseCheckCalendar = function(date_cueillette_start, date_cueillette_end) {
+let promiseCheckCalendar = function(date_cueillette_start, date_cueillette_end)
+{
         let paramsCheck = {
             timeMin: date_cueillette_start.toISOString(),
             timeMax: date_cueillette_end.toISOString(),
@@ -80,7 +84,8 @@ let promiseCheckCalendar = function(date_cueillette_start, date_cueillette_end) 
 
         return cal.Events.list(CONFIG.CalendarId, paramsCheck);
 };
-let promiseInsertCalendar = function(date_cueillette_start, date_cueillette_end, couleurPoubelle) {
+let promiseInsertCalendar = function(date_cueillette_start, date_cueillette_end, couleurPoubelle)
+{
     let paramsInsert = {
         'start': {'dateTime': date_cueillette_start},
         'end': {'dateTime': date_cueillette_end},
@@ -95,113 +100,51 @@ let promiseInsertCalendar = function(date_cueillette_start, date_cueillette_end,
 };
 
 //Promise with tasks in common
-let promiseGlobal =  new Promise((resolve, reject) =>
-    {
-        getRuesRequest(CONFIG.CivicNumber)
-            .then(streetJson => {
-                let id = streetJson.find((elem) => elem.value === CONFIG.Street).id;
-                return getCollecteInfoRequest(id);
-            })
-            .then(resolve)
-            .catch(reject)
-    });
-
-// Function to loop
-let iteration = function () {
-
-    console.log("\n/** Update of of garbage dates at "+ new Date().toISOString()+" **/");
-    //Init calendar
-    cal = new CalendarAPI(CONFIG_CALENDAR);
-
-    //Recyclage
-    let date_cueillette_recyclage_start = new Date();
-    let date_cueillette_recyclage_end = new Date();
-    promiseGlobal
-        .then(daysJson =>
-        {
-            let typeRecyclage = "recyclage";
-            let jourRecyclage = daysJson.recyclage_jour;
-
-            return getCeduleRequest(jourRecyclage, typeRecyclage);
-        })
-        .then(datesJson =>
-        {
-            // Récupération des dates
-            let date_cueillette_recyclageString = datesJson.date_cueillette;
-            let date_cueillette_recyclage = new Date(date_cueillette_recyclageString);
-
-            // MAJ dates
-            date_cueillette_recyclage.setDate(date_cueillette_recyclage.getDate() - 1);
-            date_cueillette_recyclage.setHours(14, 0, 0);
-            date_cueillette_recyclage_start = date_cueillette_recyclage;
-            date_cueillette_recyclage_end = new Date(date_cueillette_recyclage);
-            date_cueillette_recyclage_end.setHours(15, 0, 0);
-
-            return promiseCheckCalendar(date_cueillette_recyclage_start, date_cueillette_recyclage_end);
-        })
-        .then(listEvents =>
-        {
-            if (listEvents.length === 0)
-            {
-                //Insertion
-                console.log("Insertion of event recyclage at "+date_cueillette_recyclage_start.toISOString()+"...");
-                return promiseInsertCalendar(date_cueillette_recyclage_start, date_cueillette_recyclage_end, "bleu");
-            }
-            else
-            {
-                console.log("Event recyclage already existing at "+date_cueillette_recyclage_start.toISOString()+", no insertion to do...");
-                return false;
-            }
-        })
-        .then((data) =>
-        {
-            if(data !== false)
-            {
-                console.log('Insertion of event recyclage  finished');
-            }
-        })
-        .catch(error =>
-        {
-            console.error(error);
+let promiseGlobal =function()
+{
+    return getRuesRequest(CONFIG.CivicNumber)
+        .then(streetJson => {
+            let id = streetJson.find((elem) => elem.value === CONFIG.Street).id;
+            return getCollecteInfoRequest(id);
         });
+};
 
-    //Vidange
-    let date_cueillette_vidange_start = new Date();
-    let date_cueillette_vidange_end = new Date();
-    promiseGlobal
+//Workflow for one type of garbage
+let WorkFlowOneTypeOfGarbage = function(type, attributeToUse, couleurPoubelle)
+{
+    promiseGlobal()
         .then(daysJson =>
         {
-            let typeVidange = "vidange";
-            let jourVidange = daysJson.poubelle_jour;
+            let jour = daysJson[attributeToUse+"_jour"];
 
-            return getCeduleRequest(jourVidange, typeVidange);
+            return getCeduleRequest(jour, type);
         })
         .then(datesJson =>
         {
             // Récupération des dates
-            let date_cueillette_vidangeString = datesJson.date_cueillette;
-            let date_cueillette_vidange = new Date(date_cueillette_vidangeString);
+            let date_cueillette_String = datesJson.date_cueillette;
+            let date_cueillette = new Date(date_cueillette_String);
 
             // MAJ dates
-            date_cueillette_vidange.setDate(date_cueillette_vidange.getDate() - 1);
-            date_cueillette_vidange.setHours(14, 0, 0);
-            date_cueillette_vidange_start = date_cueillette_vidange;
-            date_cueillette_vidange_end = new Date(date_cueillette_vidange);
-            date_cueillette_vidange_end.setHours(15, 0, 0);
+            date_cueillette.setDate(date_cueillette.getDate() - 1);
+            date_cueillette.setHours(14, 0, 0);
+            let date_cueillette_start = date_cueillette;
+            let date_cueillette_end = new Date(date_cueillette);
+            date_cueillette_end.setHours(15, 0, 0);
 
-            return promiseCheckCalendar(date_cueillette_vidange_start, date_cueillette_vidange_end);
+            return Promise.all([promiseCheckCalendar(date_cueillette_start, date_cueillette_end), date_cueillette_start, date_cueillette_end]);
         })
-        .then(listEvents =>
+        .then(([listEvents, date_cueillette_start, date_cueillette_end])=>
         {
             if (listEvents.length === 0)
             {
                 //Insertion
-                console.log("Insertion of event vidange at "+date_cueillette_vidange_start.toISOString()+"...");
-                return promiseInsertCalendar(date_cueillette_vidange_start, date_cueillette_vidange_end, "verte");
+                console.log("Insertion of event "+type+" at "+date_cueillette_start.toISOString()+"...");
+                return promiseInsertCalendar(date_cueillette_start, date_cueillette_end, couleurPoubelle);
             }
             else
             {
-                console.log("Event vidange already existing at "+date_cueillette_vidange_start.toISOString()+", no insertion to do...");
+                console.log("Event "+type+" already existing at "+date_cueillette_start.toISOString()+", no insertion to do...");
                 return false;
             }
         })
@@ -209,7 +152,7 @@ let iteration = function () {
         {
             if(data !== false)
             {
-                console.log('Insertion of event vidange  finished');
+                console.log("Insertion of event "+type+"  finished");
             }
         })
         .catch(error =>
@@ -218,10 +161,21 @@ let iteration = function () {
         });
 };
 
+// Function to loop
+let iteration = function ()
+{
+    console.log("\n/** Update of of garbage dates at "+ new Date().toISOString()+" **/");
+    //Init calendar
+    cal = new CalendarAPI(CONFIG_CALENDAR);
+
+    WorkFlowOneTypeOfGarbage("recyclage", "recyclage", "bleu");
+    WorkFlowOneTypeOfGarbage("vidange", "poubelle", "verte");
+};
+
 // Cronjob config
 let task = new CronJob(
     {
-        cronTime: '01 * * * *',
+        cronTime: '* * * * *',
         onTick: iteration,
         start: false,
         timeZone: 'America/Los_Angeles'

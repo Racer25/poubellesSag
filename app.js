f8cc6d37ea14d05a95e53b736ac5daa45027e6a6
Charles <char-cou@hotmail.fr>
2019-04-09 17:29:27 -0400

    Ajout de la gestion de plusieurs adresses

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const request = require("request");
 const CalendarAPI = require('node-google-calendar');
 let CronJob = require('cron').CronJob;
+const nodemailer = require('nodemailer');
 
 const CONFIG_CALENDAR = require('./calendarAPI/settings');
 let cal;
 
 //CONFIG file
 const CONFIG = require('./config.json');
+
+//Preparing mail objects
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'charlescousyn@gmail.com',
+        pass: CONFIG.GooglePassword
+    }
+});
 
 /**
  * @return {boolean}
@@ -95,7 +105,7 @@ let getCeduleRequest = function (jour, type)
 };
 
 //Promises for calendar
-let promiseCheckCalendar = function(date_cueillette_start, date_cueillette_end)
+let promiseCheckCalendar = function(CalendarId, date_cueillette_start, date_cueillette_end)
 {
         let paramsCheck = {
             timeMin: date_cueillette_start.toISOString(),
@@ -105,9 +115,9 @@ let promiseCheckCalendar = function(date_cueillette_start, date_cueillette_end)
             orderBy: 'startTime'
         };
 
-        return cal.Events.list(CONFIG.CalendarId, paramsCheck);
+        return cal.Events.list(CalendarId, paramsCheck);
 };
-let promiseInsertCalendar = function(date_cueillette_start, date_cueillette_end, couleurPoubelle)
+let promiseInsertCalendar = function(CalendarId, date_cueillette_start, date_cueillette_end, couleurPoubelle)
 {
     let paramsInsert = {
         'start': {'dateTime': date_cueillette_start},
@@ -119,64 +129,136 @@ let promiseInsertCalendar = function(date_cueillette_start, date_cueillette_end,
         'colorId': 1
     };
 
-    return cal.Events.insert(CONFIG.CalendarId, paramsInsert);
+    return cal.Events.insert(CalendarId, paramsInsert);
+};
+
+//Promise to send mails
+let promiseSendMail = function(mailOptions)
+{
+    return new Promise((resolve, reject) =>
+    {
+        transporter.sendMail(mailOptions,
+            function (err, info)
+            {
+                if (err)
+                {
+                    reject(err)
+                }
+                else
+                {
+                    resolve(info);
+                }
+            }
+        )});
 };
 
 //Promise with tasks in common
-let promiseGlobal =function()
+let promiseGlobal = function()
 {
-    return getRuesRequest(CONFIG.CivicNumber)
-        .then(streetJson => {
-            let id = streetJson.find((elem) => elem.value === CONFIG.Street).id;
-            return getCollecteInfoRequest(id);
-        });
+    let adresses = CONFIG.Adresses;
+
+    let promisesGetRueRequest = adresses.map(adress =>
+        getRuesRequest(adress.CivicNumber)
+                .then(streetJson =>
+                {
+                    let id = streetJson.find((elem) => elem.value === adress.Street).id;
+                    return Promise.all([getCollecteInfoRequest(id), adress]);
+                }));
+
+    return Promise.all(promisesGetRueRequest);
 };
 
 //Workflow for one type of garbage
 let WorkFlowOneTypeOfGarbage = function(type, attributeToUse, couleurPoubelle)
 {
     promiseGlobal()
-        .then(daysJson =>
+        .then(daysJsonTab =>
         {
-            let jour = daysJson[attributeToUse+"_jour"];
-
-            return getCeduleRequest(jour, type);
-        })
-        .then(datesJson =>
-        {
-            // Récupération des dates
-            let date_cueillette_String = datesJson.date_cueillette;
-            let date_cueillette = new Date(date_cueillette_String);
-
-            // MAJ dates
-            date_cueillette.setDate(date_cueillette.getDate() - 1);
-            date_cueillette.setHours(14, 0, 0);
-            let date_cueillette_start = date_cueillette;
-            let date_cueillette_end = new Date(date_cueillette);
-            date_cueillette_end.setHours(15, 0, 0);
-
-            return Promise.all([promiseCheckCalendar(date_cueillette_start, date_cueillette_end), date_cueillette_start, date_cueillette_end]);
-        })
-        .then(([listEvents, date_cueillette_start, date_cueillette_end])=>
-        {
-            if (listEvents.length === 0)
+            let promisesGetCeduleRequest = daysJsonTab.map(daysJson =>
             {
-                //Insertion
-                console.log("Insertion of event "+type+" at "+date_cueillette_start.toISOString()+"...");
-                return promiseInsertCalendar(date_cueillette_start, date_cueillette_end, couleurPoubelle);
-            }
-            else
-            {
-                console.log("Event "+type+" already existing at "+date_cueillette_start.toISOString()+", no insertion to do...");
-                return false;
-            }
+                let jour = daysJson[0][attributeToUse+"_jour"];
+
+                return Promise.all([getCeduleRequest(jour, type), daysJson[1]]);
+            });
+
+            return Promise.all(promisesGetCeduleRequest);
         })
-        .then((data) =>
+        .then(datesJsonTab =>
         {
-            if(data !== false)
+            let promises = datesJsonTab.map(datesJson =>
             {
-                console.log("Insertion of event "+type+"  finished");
-            }
+                // Récupération des dates
+                let date_cueillette_String = datesJson[0].date_cueillette;
+                let date_cueillette = new Date(date_cueillette_String);
+
+                // MAJ dates
+                date_cueillette.setDate(date_cueillette.getDate() - 1);
+                date_cueillette.setHours(14, 0, 0);
+                let date_cueillette_start = date_cueillette;
+                let date_cueillette_end = new Date(date_cueillette);
+                date_cueillette_end.setHours(15, 0, 0);
+
+                let adress = datesJson[1];
+                if(adress.MailNotCalendar)
+                {
+                    let dateNow = new Date();
+
+                    //Si on est la veille du passage et qu'il est entre 14h et 15h
+                    if(dateNow.getFullYear() === date_cueillette.getFullYear() &&
+                        dateNow.getMonth() === date_cueillette.getMonth() &&
+                        dateNow.getDate() === date_cueillette.getDate() &&
+                        dateNow.getHours() > 14 && dateNow.getHours() < 15)
+                    {
+                        //Envoyer mail
+
+                        //Init html of the mail
+                        let myHtml="<div><p>Il faut sortir la poubelle "+couleurPoubelle+" !!</p></div>";
+
+                        let mailOptions = {
+                            from: 'charlescousyn@gmail.com', // sender address
+                            to: adress.Mail, // list of receivers
+                            subject: 'Passage de la poubelle '+couleurPoubelle+" demain", // Subject line
+                            html: myHtml
+                        };
+
+                        promiseSendMail(mailOptions)
+                            .then(console.log)
+                            .catch(console.error);
+                    }
+                }
+                else
+                {
+                    //Exécution du Calendrier
+                    return Promise.all([promiseCheckCalendar(adress.CalendarId, date_cueillette_start, date_cueillette_end), date_cueillette_start, date_cueillette_end])
+                        .then(([listEvents, date_cueillette_start, date_cueillette_end])=>
+                        {
+                            if (listEvents.length === 0)
+                            {
+                                //Insertion
+                                console.log("Insertion of event "+type+" at "+date_cueillette_start.toISOString()+"...");
+                                return promiseInsertCalendar(adress.CalendarId, date_cueillette_start, date_cueillette_end, couleurPoubelle);
+                            }
+                            else
+                            {
+                                console.log("Event "+type+" already existing at "+date_cueillette_start.toISOString()+", no insertion to do...");
+                                return false;
+                            }
+                        })
+                        .then((data) =>
+                        {
+                            if(data !== false)
+                            {
+                                console.log("Insertion of event "+type+"  finished");
+                            }
+                        })
+                        .catch(error =>
+                        {
+                            console.error(error);
+                        });
+                }
+
+            });
+            return Promise.all(promises);
         })
         .catch(error =>
         {
